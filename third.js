138d6bcbed57366d71754b12f540268c4d0b66e7
keilafuentes <keiladevs@gmail.com>
2022-11-24 22:48:09 -0400

    Add third exercise

---
+++ third.js
@@ -0,0 +1,9 @@
+//Read an integer and determine if it is negative.
+
+let number = prompt("Enter a number", "Type a number here");
+
+if (number < 0) {
+  alert("This number IS negative");
+} else {
+  alert("This number IS NOT negative");
+}
