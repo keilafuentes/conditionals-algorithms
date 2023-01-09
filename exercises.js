20cd61a160abdb1c8a768a9e609f67b58b4655e0
keilafuentes <keiladevs@gmail.com>
2023-01-09 15:55:59 -0400

    Add exercise 31

--- exercises.js
+++ exercises.js
@@ -2,3 +2,14 @@
 function negative(number) {
   return number < 0;
 }
+
+function endFour(number) {}
+
+function getLastDigit(number) {}
+
+function getFirstDigit(number) {}
+
+//31
+function isTen(number) {
+  return number == 10;
+}
